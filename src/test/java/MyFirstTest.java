484957a95a776f07b7fc0646f8df670e857f2fd6
patric noriega <patricnoriega@gmail.com>
2021-11-15 11:42:18 -0600

    myFirstTest complete

---
+++ src/test/java/MyFirstTest.java
@@ -0,0 +1,47 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import static org.junit.Assert.*;
+
+public class MyFirstTest {
+
+    @Test
+    public void testingCodeup() {
+        String expected = "Codeup";
+        String actual = "Codeup";
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testingArrayList() {
+        List<String> languages = new ArrayList<>();
+        List<String> moreLanguages = new ArrayList<>();
+
+        assertNotSame(languages, moreLanguages);
+    }
+
+    @Test
+    public void testingArray() {
+        int[] numbers = {1, 2, 3};
+        int[] otherNumbers = new int[3];
+        otherNumbers[0] = 1;
+        otherNumbers[1] = 2;
+        otherNumbers[2] = 3;
+
+        assertArrayEquals(numbers, otherNumbers);
+    }
+
+    @Test
+    public void testingTrueOFalse() {
+        String language = "PHP";
+        language.contains("H"); // use assertTrue for this statement
+        language.contains("J"); // use assertFalse for this statement
+
+        assertFalse("String language does not contain char J",language.contains("J"));
+        assertTrue("String language does contain char H ", language.contains("H"));
+    }
+}
